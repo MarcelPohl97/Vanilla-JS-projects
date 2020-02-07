192a3a4eb4bc7b7b31eeb204903be3a38ebc27a9
Marcel <Marcel.Pohl1234@gmx.de>
2020-02-08 00:53:50 +0100

    added animation

--- 1. Form Validator/script.js
+++ 1. Form Validator/script.js
@@ -1,1 +1,6 @@
-alert("Hello")
+document.getElementById("register-animation").addEventListener("click", function(){
+    document.getElementById("right-animation").style.opacity = "1";
+    document.getElementById("right-animation").style.width = "50%";
+    document.getElementById("left-animation").style.width = "50%";
+
+  });
