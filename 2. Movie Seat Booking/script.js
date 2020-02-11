9ca42361d532a4b82799d7ddd1bc9e6fcd4512bb
Marcel <45712008+MarcelPohl97@users.noreply.github.com>
2020-02-11 20:29:24 +0100

    Added api url and form animation

--- 2. Movie Seat Booking/script.js
+++ 2. Movie Seat Booking/script.js
@@ -6,5 +6,30 @@ container.addEventListener('click', e => {
       !e.target.classList.contains('taken')
     ) {
       e.target.classList.toggle('selection');
+      update_selected_seats();
     }
   });
+
+  let update_selected_seats = () => {
+    const seats_counter = document.querySelectorAll(".selection");
+    const selected_seats = document.getElementById("seat-counter");
+    selected_seats.innerHTML = seats_counter.length;
+  }
+
+const movie_searcher_form = document.getElementById("movie-searcher-form")
+
+movie_searcher_form.addEventListener('click', e => {
+  movie_searcher_form.classList.toggle("movie-animate-form")
+})
+
+//Animate function
+  const animate_form = () => {
+    const movie_form = document.getElementById("movie-searcher");
+    movie_form.style.width = "30vw";
+    movie_form.style.height = "100vh";
+    movie_form.style.borderTopRightRadius = "25%";
+    movie_form.style.borderBottomRightRadius = "25%";
+    movie_form.h1.style.opacity = "0";
+  }
+
+  let fetch_url = "http://www.omdbapi.com/?apikey=75046c65&t=riverdale"
