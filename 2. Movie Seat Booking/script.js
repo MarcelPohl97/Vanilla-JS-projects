2ef5849ca382929e1cc8beb5fdad113eaff32143
Marcel <45712008+MarcelPohl97@users.noreply.github.com>
2020-02-13 21:06:49 +0100

    Added api fetch function, changing title plot and img

--- 2. Movie Seat Booking/script.js
+++ 2. Movie Seat Booking/script.js
@@ -10,28 +10,40 @@ container.addEventListener('click', e => {
     }
   });
 
-  let update_selected_seats = () => {
+  const update_selected_seats = () => {
     const seats_counter = document.querySelectorAll(".selection");
     const selected_seats = document.getElementById("seat-counter");
     selected_seats.innerHTML = seats_counter.length;
-  }
+  };
 
 const movie_searcher_form = document.getElementById("movie-searcher-form")
 const movie_searcher_form_animator = document.getElementById("movie-search-animate")
+const movie_form_activator = document.getElementById("movie-search-input-show-hide")
 
 movie_searcher_form_animator.addEventListener('click', () => {
   movie_searcher_form.classList.toggle("movie-animate-form");
+  movie_form_activator.classList.toggle("fade");
+});
 
-})
+const movie_btn_fetch = document.getElementById("fetch-movie")
+const movie_input = document.getElementById("movie-input")
 
-//Animate function
-  const animate_form = () => {
-    const movie_form = document.getElementById("movie-searcher");
-    movie_form.style.width = "100vw";
-    movie_form.style.height = "100vh";
-    movie_form.style.borderTopRightRadius = "0%";
-    movie_form.style.borderBottomRightRadius = "0%";
-    movie_form.h1.style.opacity = "0";
-  }
+const poster = document.getElementById("poster")
+const title = document.getElementById("title")
+const plot = document.getElementById("plot")
 
-  let fetch_url = "http://www.omdbapi.com/?apikey=75046c65&t=riverdale"
+movie_btn_fetch.addEventListener("click", () => {
+  get_movie(movie_input.value)
+});
+
+function get_movie(movie){
+  fetch(`http://www.omdbapi.com/?apikey=75046c65&t=${movie}`).then((response) => {
+            return response.json();
+        }).then(data => {
+            poster.src = data["Poster"]
+            title.innerHTML = data["Title"]
+            plot.innerHTML = data["Plot"]
+        }).catch((err) => {
+            console.log("rejected", err);
+        })  
+    }
