2cf3beaf5f6b795e96873e15eafec2ca0935a497
Marcel <Marcel.Pohl1234@gmx.de>
2020-04-29 00:37:44 +0200

    Added Dragg function, generate Card

--- 7. Mastermind Realtime Collaboration Brainstorming/app.js
+++ 7. Mastermind Realtime Collaboration Brainstorming/app.js
@@ -1,10 +1,14 @@
 const container = document.querySelector('.container');
-const container2 = document.querySelectorAll('.container');
 let container_Width = document.getElementById("container").offsetWidth;
 let container_Height = document.getElementById("container").offsetHeight;
 let moving = false;
+let initX;
+let initY;
+let firstX; 
+let firstY;
 
-
+const add_Card = document.getElementById('header__add-card')
+const card_Input = document.getElementById('header__input')
 
 
 class Card {
@@ -18,7 +22,7 @@ class Card {
         this.element.innerHTML = text;
         this.element.classList = 'card';
         this.element.contentEditable = 'true';
-        this.element.draggable = 'true';
+        
         this.xvel = Math.random() * (4 - 2) - 1;
         this.yvel = Math.random() * (4 - 2) - 1;
         container.appendChild(this.element);
@@ -66,10 +70,6 @@ class Card {
 
 let cards = [];
 
-for(i = 0; i < 10; i++){
-    cards.push(new Card("Hello"))
-}
-
 let animate = () => {
     cards.forEach(element =>  {
         element.move();
@@ -79,4 +79,57 @@ let animate = () => {
 setInterval(function(){
     animate();
 }, 10);
+
+container.addEventListener('mousedown', function(e) {
+    if(e.target.classList.contains("card")){
+	    initX = e.target.offsetLeft;
+	    initY = e.target.offsetTop;
+        firstX = e.pageX;
+        firstY = e.pageY;
+        e.target.addEventListener('mousemove', dragIt, false);
+
+    }
+
+	container.addEventListener('mouseup', e => {
+        if(e.target.classList.contains('card')){
+            e.target.removeEventListener('mousemove', dragIt, false);
+        }
+	}, false);
+
+}, false);
+
+container.addEventListener('touchstart', function(e) {
+    if(e.target.classList.contains("card")){
+        initX = e.target.offsetLeft;
+        initY = e.target.offsetTop;
+        var touch = e.touches;
+        firstX = touch[0].pageX;
+        firstY = touch[0].pageY;
+
+        e.target.addEventListener('touchmove', swipeIt, false);
+    }
+
+	container.addEventListener('touchend', function(e) {
+        if(e.target.classList.contains("card")) {
+		    e.target.removeEventListener('touchmove', swipeIt, false);
+        }
+	}, false);
+
+}, false);
+
+function dragIt(e) {
+	this.style.left = initX+e.pageX-firstX + 'px';
+	this.style.top = initY+e.pageY-firstY + 'px';
+};
+
+function swipeIt(e) {
+	let contact = e.touches;
+	this.style.left = initX+contact[0].pageX-firstX + 'px';
+	this.style.top = initY+contact[0].pageY-firstY + 'px';
+};
+
+add_Card.addEventListener('click', () => {
+    cards.push(new Card(card_Input.value));
+    card_Input.value = "";
+})
 
